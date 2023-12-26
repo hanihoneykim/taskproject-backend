f20d61707c2b4413a837484fd8dcdfffa272fb98
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-12-26 16:25:35 +0900

    add: urls

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from taskapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+    path("", views.index, name="index"),
+    path("previous/", views.index, name="previous"),
+    path("task/", views.index, name="create-task"),
+    path("task/<int:task_id>/delete/", views.index, name="delete-task"),
+    path("task/<int:task_id>/item/", views.index, name="create-item"),
+    path("task/<int:task_id>/item/<int:check_id>/", views.index, name="check-item"),
+    path(
+        "task/<int:task_id>/item/<int:check_id>/delete/",
+        views.index,
+        name="delete-item",
+    ),
+    path("task/<int:task_id>/", views.index, name="view-item"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
