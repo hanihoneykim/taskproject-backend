f9042d336aaa774f635247e2d92cc302bb7595ec
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-12-26 17:13:16 +0900

    add: ChecklistItem model

--- taskapp/models.py
+++ taskapp/models.py
@@ -16,3 +16,10 @@ class Task(models.Model):
     )
     due = models.DateTimeField(null=False)
     create_at = models.DateTimeField(auto_now_add=True, null=False)
+
+
+class ChecklistItem(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100, null=False)
+    is_checked = models.BooleanField(null=False, default=False)
+    create_at = models.DateTimeField(auto_now_add=True, null=False)
