a851dc3ee20c64d9ca4b164b15e730d10ba79ed2
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-12-26 16:31:56 +0900

    add: Task model

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    class TaskType(models.TextChoices):
+        JOB = "JOB", _("업무")
+        HEALTH = "HEALTH", _("건강")
+        PERSONAL = "PERSONAL", _("개인")
+
+    title = models.CharField(max_length=50, null=False)
+    type = models.CharField(
+        choices=TaskType.choices,
+        max_length=15,
+        default=TaskType.JOB,
+    )
+    due = models.DateTimeField(null=False)
+    create_at = models.DateTimeField(auto_now_add=True, null=False)
